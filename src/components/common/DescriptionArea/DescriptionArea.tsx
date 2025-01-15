6a8d3b3cad9e9f55cb4a903469f373062534e76c
Sonesh Khatry <skhatry@informeddna.com>
2025-01-16 00:43:41 +0545

    Add ellipsis for card description

--- src/components/common/DescriptionArea/DescriptionArea.tsx
+++ src/components/common/DescriptionArea/DescriptionArea.tsx
@@ -5,7 +5,17 @@ interface DescriptionAreaProps {
 }
 const DescriptionArea = (props: DescriptionAreaProps) => {
   return (
-    <Typography sx={{ color: '#ffffff' }} variant="body1">
+    <Typography
+      sx={{
+        color: '#ffffff',
+        display: '-webkit-box',
+        WebkitLineClamp: 6, // Number of lines
+        WebkitBoxOrient: 'vertical',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      }}
+      variant="body1"
+    >
       {props.description}
     </Typography>
   )
