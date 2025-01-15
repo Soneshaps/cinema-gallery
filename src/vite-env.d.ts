ad45296ae0eea91e7b8a9d5f651c7652fd99a799
Sonesh Khatry <skhatry@informeddna.com>
2025-01-16 01:16:43 +0545

    fix: Add TypeScript types for import.meta.env in Vite project

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 // / <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_URL: string
+  readonly VITE_TMDB_API_KEY: string
+  // Add more variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
